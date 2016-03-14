3fee8c867d0c0b8b40ef89b90d16d5c08f83efd3
erinboyle05 <erinboyle05@gmail.com>
2016-03-14 16:59:03 -0400

    Begin code to fix misspelled values.

--- Coding.R
+++ Coding.R
@@ -17,24 +17,41 @@ storm_data <- read.csv("storm_data.csv.bz2")
 copy_data <- storm_data
 
 storm_data$EVTYPE <- storm_data$EVTYPE %>%
-        as.character() %>%
-        toupper()
+        # as.character() %>%
+        toupper() %>%
+        as.factor()
 
 storm_data$EVTYPE <- gsub("/"," ", storm_data$EVTYPE) 
 
-test_data <- count(storm_data, EVTYPE)
+test_data <- storm_data %>%
+        group_by(EVTYPE) %>%
+        summarize(FATALITIES = sum(FATALITIES), INJURIES = sum(INJURIES), Total = sum(FATALITIES, INJURIES)) %>%
+        filter(FATALITIES > 0 | INJURIES > 0)
+
+storm_data$EVTYPE <- gsub(".*TORNADO.*", "TORNADO", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*THUNDERST.*", "THUNDERSTORM", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*TSTM.*", "THUNDERSTORM", storm_data$EVTYPE)
+
+# from <- list(c("TORNADO F2","TORNADO F3","TORNADOES, TSTM WIND, HAIL"),
+#              c("DROUGHT EXCESSIVE HEAT","HYPERTHERMIA EXPOSURE","RECORD EXCESSIVE HEAT"),
+#              C())
+# to <- c("TORNADO", "EXCESSIVE HEAT")
+# 
+# find.in.list <- function(x, y) match(TRUE, sapply(y, `%in%`, x = x))
+# idx.in.list  <- sapply(levels(storm_data$EVTYPE), find.in.list, from)
+# levels(storm_data$EVTYPE)  <- ifelse(is.na(idx.in.list), levels(storm_data$EVTYPE), to[idx.in.list])
 
 q1_data <- storm_data %>%
         group_by(EVTYPE) %>%
         summarize(FATALITIES = mean(FATALITIES), INJURIES = mean(INJURIES), Total = sum(FATALITIES, INJURIES)) %>%
         filter(FATALITIES > 0 | INJURIES > 0) %>%
-        top_n(10, Total) %>% 
+        top_n(10, Total) %>%
         select(-Total) %>%
         melt(id.var = "EVTYPE")
 
-q1_plot <- ggplot(q1_data, aes(x = EVTYPE, y = value, fill = variable)) +
-        geom_bar(stat = "identity")+
-        theme(axis.text.x = element_text(angle = 90, hjust = 1))
-
-print(q1_plot)
+# q1_plot <- ggplot(q1_data, aes(x = EVTYPE, y = value, fill = variable)) +
+#         geom_bar(stat = "identity") +
+#         theme(axis.text.x = element_text(angle = 90, hjust = 1))
+# 
+# print(q1_plot)
 
