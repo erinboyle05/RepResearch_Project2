7582b7424b17e90219dba4b55eb982d8f6bbb26e
erinboyle05 <erinboyle05@gmail.com>
2016-03-15 16:23:55 -0400

    Finish code for human cost Begin code for financial cost

--- Coding.R
+++ Coding.R
@@ -24,45 +24,84 @@ storm_data$EVTYPE <- storm_data$EVTYPE %>%
 storm_data$EVTYPE <- gsub("/"," ", storm_data$EVTYPE) 
 
 storm_data$EVTYPE <- gsub(".*TORNADO.*", "TORNADO", storm_data$EVTYPE)
-storm_data$EVTYPE <- gsub(".*THUNDERST.*", "THUNDERSTORM", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*THUNDER.*", "THUNDERSTORM", storm_data$EVTYPE)
 storm_data$EVTYPE <- gsub(".*TSTM.*", "THUNDERSTORM", storm_data$EVTYPE)
 storm_data$EVTYPE <- gsub(".*AVALA.*", "AVALANCHE", storm_data$EVTYPE)
-storm_data$EVTYPE <- gsub(".*FLASH.*", "FLASH FLOOD", storm_data$EVTYPE)
 storm_data$EVTYPE <- gsub(".*LIGHTN.*", "LIGHTNING", storm_data$EVTYPE)
 storm_data$EVTYPE <- gsub("FLOOD.*", "FLOOD", storm_data$EVTYPE)
-storm_data$EVTYPE <- gsub(".*WINTE.*", "WINTER STORM", storm_data$EVTYPE)
-storm_data$EVTYPE <- gsub(".*HURRICANE.*", "HURRICANE", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*FLASH.*", "FLASH FLOOD", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*WINT.*", "WINTER STORM", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*HURR.*", "HURRICANE", storm_data$EVTYPE)
 storm_data$EVTYPE <- gsub(".*TYPHOON.*", "HURRICANE", storm_data$EVTYPE)
 storm_data$EVTYPE <- gsub(".*SNOW.*", "WINTER STORM", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*TROPI.*", "TROPICAL STORM", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*FIRE.*", "WILDFIRE", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*FOG.*", "DENSE FOG", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*MARINE.*", "MARINE", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*WIND.*", "HIGH WIND", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*COLD.*", "COLD", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*HEAT.*", "HEAT", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*BLACK.*", "WINTER WEATER", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub("ICE.*", "WINTER WEATHER", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub("ICY.*", "WINTER WEATHER", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*SURF.*", "HIGH SURF", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*WARM.*", "HEAT", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*GLAZE.*", "WINTER WEATHER", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*ICE.*", "WINTER WEATHER", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*FREEZING.*", "WINTER WEATHER", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*RAIN.*", "HEAVY RAIN", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*SEAS.*", "HIGH SURF", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*CURRENT.*", "RIP CURRENT", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*HYPO.*", "COLD", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*HYPER.*", "HEAT", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*RISING.*", "FLASH FLOOD", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*COASTAL.*", "MARINE", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*LOW.*", "COLD", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*MIXED.*", "SLEET", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*ROGUE.*", "MARINE", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*HIGH WA.*", "FLOOD", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*DROWNING.*", "OTHER", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*SWEL.*", "HIGH SURF", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*SWEL.*", "HIGH SURF", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*SLIDE.*", "OTHER", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*MICRO.*", "HIGH WIND", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*FLD.*", "FLOOD", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*TIDE.*", "STORM SURGE", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*FREEZE.*", "FROST/FLOOD", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*FROST.*", "FROST/FLOOD", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*HAIL.*", "HAIL", storm_data$EVTYPE)
+storm_data <- storm_data[!(storm_data$EVTYPE == "HIGH"), ]
 
-# from <- list(".*TORNADO.*", ".*THUNDERST.*", ".*TSTM.*",".*AVALA.*", ".*FLASH.*",
-#       ".*LIGHTN.*", "FLOOD.*", ".*WINTE.*", ".*HURRICANE.*", ".*TYPHOON.*",".*SNOW.*"
-#             )
-# to <- c("TORNADO","THUNDERSTORM","THUNDERSTORM","AVALANCHE","FLASH FLOOD","LIGHTNING",
-#       "FLOOD", "WINTER STORM", "HURRICANE","HURRICANE", "WINTER STORM")
-# 
-# find.in.list <- function(x, y) match(TRUE, sapply(y, `%in%`, x = x))
-# idx.in.list  <- sapply(levels(storm_data$EVTYPE), find.in.list, from)
-# levels(storm_data$EVTYPE)  <- ifelse(is.na(idx.in.list), levels(storm_data$EVTYPE), to[idx.in.list])
+# Human cost
 
+hstorm_data <- storm_data[!(storm_data$FATALITIES > 0 & storm_data$INJURIES > 0),]
 
-
-test_data <- storm_data %>%
+htest_data <- hstorm_data %>%
         group_by(EVTYPE) %>%
-        summarize(FATALITIES = sum(FATALITIES), INJURIES = sum(INJURIES), Total = sum(FATALITIES, INJURIES)) %>%
-        filter(FATALITIES > 0 | INJURIES > 0)
+        filter(FATALITIES > 0 | INJURIES > 0) %>%
+        summarize(FATALITIES = sum(FATALITIES), INJURIES = sum(INJURIES), Total = sum(FATALITIES, INJURIES)) 
 
-q1_data <- storm_data %>%
+
+
+q1_data <- hstorm_data %>%
         group_by(EVTYPE) %>%
-        summarize(FATALITIES = mean(FATALITIES), INJURIES = mean(INJURIES), Total = sum(FATALITIES, INJURIES)) %>%
+        summarize(FATALITIES = mean(FATALITIES), INJURIES = mean(INJURIES), Total = mean(FATALITIES + INJURIES)) %>%
         filter(FATALITIES > 0 | INJURIES > 0) %>%
         top_n(10, Total) %>%
         select(-Total) %>%
         melt(id.var = "EVTYPE")
 
-# q1_plot <- ggplot(q1_data, aes(x = EVTYPE, y = value, fill = variable)) +
-#         geom_bar(stat = "identity") +
-#         theme(axis.text.x = element_text(angle = 90, hjust = 1))
-# 
-# print(q1_plot)
+q1_plot <- ggplot(q1_data, aes(x = EVTYPE, y = value, fill = variable)) +
+        geom_bar(stat = "identity") +
+        theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
+print(q1_plot)
+
+# Financial cost
+
+fstorm_data <- storm_data[!(storm_data$PROPDMG > 0 & storm_data$CROPDMG > 0),]
+
+ftest_data <- fstorm_data %>%
+        group_by(EVTYPE) %>%
+        filter(FATALITIES > 0 | INJURIES > 0) %>%
+        summarize(FATALITIES = sum(FATALITIES), INJURIES = sum(INJURIES), Total = sum(FATALITIES, INJURIES)) 
