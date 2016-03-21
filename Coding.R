9b5b706196d67731e2dec51c35337d4740744b39
erinboyle05 <erinboyle05@gmail.com>
2016-03-21 17:05:06 -0400

    Even more coding...

--- Coding.R
+++ Coding.R
@@ -17,9 +17,11 @@ storm_data <- read.csv("storm_data.csv.bz2")
 copy_data <- storm_data
 
 storm_data$EVTYPE <- storm_data$EVTYPE %>%
-        # as.character() %>%
         toupper() %>%
         as.factor()
+
+storm_data$PROPDMGEXP <- toupper(storm_data$PROPDMGEXP)
+storm_data$CROPDMGEXP <- toupper(storm_data$CROPDMGEXP)
 
 storm_data$EVTYPE <- gsub("/"," ", storm_data$EVTYPE) 
 
@@ -41,7 +43,7 @@ storm_data$EVTYPE <- gsub(".*MARINE.*", "MARINE", storm_data$EVTYPE)
 storm_data$EVTYPE <- gsub(".*WIND.*", "HIGH WIND", storm_data$EVTYPE)
 storm_data$EVTYPE <- gsub(".*COLD.*", "COLD", storm_data$EVTYPE)
 storm_data$EVTYPE <- gsub(".*HEAT.*", "HEAT", storm_data$EVTYPE)
-storm_data$EVTYPE <- gsub(".*BLACK.*", "WINTER WEATER", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*BLACK.*", "WINTER WEATHER", storm_data$EVTYPE)
 storm_data$EVTYPE <- gsub("ICE.*", "WINTER WEATHER", storm_data$EVTYPE)
 storm_data$EVTYPE <- gsub("ICY.*", "WINTER WEATHER", storm_data$EVTYPE)
 storm_data$EVTYPE <- gsub(".*SURF.*", "HIGH SURF", storm_data$EVTYPE)
@@ -62,23 +64,59 @@ storm_data$EVTYPE <- gsub(".*ROGUE.*", "MARINE", storm_data$EVTYPE)
 storm_data$EVTYPE <- gsub(".*HIGH WA.*", "FLOOD", storm_data$EVTYPE)
 storm_data$EVTYPE <- gsub(".*DROWNING.*", "OTHER", storm_data$EVTYPE)
 storm_data$EVTYPE <- gsub(".*SWEL.*", "HIGH SURF", storm_data$EVTYPE)
-storm_data$EVTYPE <- gsub(".*SWEL.*", "HIGH SURF", storm_data$EVTYPE)
-storm_data$EVTYPE <- gsub(".*SLIDE.*", "OTHER", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*CSTL.*", "COASTAL FLOOD", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*SLIDE.*", "DEBRIS FLOW", storm_data$EVTYPE)
 storm_data$EVTYPE <- gsub(".*MICRO.*", "HIGH WIND", storm_data$EVTYPE)
 storm_data$EVTYPE <- gsub(".*FLD.*", "FLOOD", storm_data$EVTYPE)
-storm_data$EVTYPE <- gsub(".*TIDE.*", "STORM SURGE", storm_data$EVTYPE)
-storm_data$EVTYPE <- gsub(".*FREEZE.*", "FROST/FLOOD", storm_data$EVTYPE)
-storm_data$EVTYPE <- gsub(".*FROST.*", "FROST/FLOOD", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*TID.*", "STORM SURGE", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*FREEZE.*", "FROST/FREEZE", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*FROST.*", "FROST/FREEZE", storm_data$EVTYPE)
 storm_data$EVTYPE <- gsub(".*HAIL.*", "HAIL", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*RIVER.*", "FLOOD", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*MINOR.*", "FLOOD", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*STREAM.*", "FLOOD", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*WETNESS.*", "HEAVY RAIN", storm_data$EVTYPE)
+storm_data$EVTYPE <- gsub(".*MAJOR.*", "FLOOD", storm_data$EVTYPE)
 storm_data <- storm_data[!(storm_data$EVTYPE == "HIGH"), ]
+
+storm_data$PROPDMGEXP <- gsub("0", "1", storm_data$PROPDMGEXP)
+storm_data$PROPDMGEXP <- gsub("B", "1000000", storm_data$PROPDMGEXP)
+storm_data$PROPDMGEXP <- gsub("K", "1000", storm_data$PROPDMGEXP)
+storm_data$PROPDMGEXP <- gsub("M", "100000", storm_data$PROPDMGEXP)
+storm_data$PROPDMGEXP <- gsub("H", "100", storm_data$PROPDMGEXP)
+storm_data$PROPDMGEXP <- gsub("2", "100", storm_data$PROPDMGEXP)
+storm_data$PROPDMGEXP <- gsub("3", "1000", storm_data$PROPDMGEXP)
+storm_data$PROPDMGEXP <- gsub("4", "10000", storm_data$PROPDMGEXP)
+storm_data$PROPDMGEXP <- gsub("5", "100000", storm_data$PROPDMGEXP)
+storm_data$PROPDMGEXP <- gsub("6", "1000000", storm_data$PROPDMGEXP)
+storm_data$PROPDMGEXP <- gsub("7", "10000000", storm_data$PROPDMGEXP)
+storm_data$PROPDMGEXP <- gsub("-", "1", storm_data$PROPDMGEXP)
+# storm_data$PROPDMGEXP <- gsub("", "1", storm_data$PROPDMGEXP)
+
+storm_data$CROPDMGEXP <- gsub("0", "1", storm_data$CROPDMGEXP)
+storm_data$CROPDMGEXP <- gsub("B", "1000000", storm_data$CROPDMGEXP)
+storm_data$CROPDMGEXP <- gsub("K", "1000", storm_data$CROPDMGEXP)
+storm_data$CROPDMGEXP <- gsub("M", "100000", storm_data$CROPDMGEXP)
+# storm_data$CROPDMGEXP <- gsub("", "1", storm_data$CROPDMGEXP)
+
+# storm_data$CROPDMGEXP <- as.factor(storm_data$CROPDMGEXP)
+storm_data$CROPDMGEXP <- as.numeric(storm_data$CROPDMGEXP)
+# storm_data$PROPDMGEXP <- as.factor(storm_data$PROPDMGEXP)
+storm_data$PROPDMGEXP <- as.numeric(storm_data$PROPDMGEXP)
+
+storm_data$PROPDMGEXP[is.na(storm_data$PROPDMGEXP)] <- 1
+storm_data$CROPDMGEXP[is.na(storm_data$CROPDMGEXP)] <- 1
+
+test_data <- storm_data %>%
+        group_by(EVTYPE) %>%
+        summarize(FATALITIES = sum(FATALITIES), INJURIES = sum(INJURIES), Total = sum(FATALITIES, INJURIES))
 
 # Human cost
 
-hstorm_data <- storm_data[!(storm_data$FATALITIES > 0 & storm_data$INJURIES > 0),]
+hstorm_data <- storm_data[!(storm_data$FATALITIES == 0 & storm_data$INJURIES == 0),]
 
 htest_data <- hstorm_data %>%
         group_by(EVTYPE) %>%
-        filter(FATALITIES > 0 | INJURIES > 0) %>%
         summarize(FATALITIES = sum(FATALITIES), INJURIES = sum(INJURIES), Total = sum(FATALITIES, INJURIES)) 
 
 
@@ -86,7 +124,6 @@ htest_data <- hstorm_data %>%
 q1_data <- hstorm_data %>%
         group_by(EVTYPE) %>%
         summarize(FATALITIES = mean(FATALITIES), INJURIES = mean(INJURIES), Total = mean(FATALITIES + INJURIES)) %>%
-        filter(FATALITIES > 0 | INJURIES > 0) %>%
         top_n(10, Total) %>%
         select(-Total) %>%
         melt(id.var = "EVTYPE")
@@ -99,9 +136,25 @@ print(q1_plot)
 
 # Financial cost
 
-fstorm_data <- storm_data[!(storm_data$PROPDMG > 0 & storm_data$CROPDMG > 0),]
+fstorm_data <- storm_data[!(storm_data$PROPDMG == 0 & storm_data$CROPDMG == 0),]
 
-ftest_data <- fstorm_data %>%
+        
+        
+ptest_data <- fstorm_data %>%
+        group_by(CROPDMGEXP) %>%
+        count(CROPDMGEXP)
+        
+q2_data <- fstorm_data %>%
+        mutate(CROPDMG = (CROPDMG*CROPDMGEXP)/10^6) %>%
+        mutate(PROPDMG = (PROPDMG*PROPDMGEXP)/10^6) %>%
         group_by(EVTYPE) %>%
-        filter(FATALITIES > 0 | INJURIES > 0) %>%
-        summarize(FATALITIES = sum(FATALITIES), INJURIES = sum(INJURIES), Total = sum(FATALITIES, INJURIES)) 
+        summarise(PROPDMG = mean(PROPDMG), CROPDMG = mean(CROPDMG), Total = mean(PROPDMG + CROPDMG)) %>%
+        top_n(10, Total) %>%
+        select(-Total) %>%
+        melt(id.var = "EVTYPE")
+
+q2_plot <- ggplot(q2_data, aes(x = EVTYPE, y = value, fill = variable)) +
+        geom_bar(stat = "identity") +
+        theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+print(q2_plot)
